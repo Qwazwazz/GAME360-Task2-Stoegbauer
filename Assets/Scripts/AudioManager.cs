47d362c7df7c642005828228cf5b41087b286776
Qwazwazz <mstoe001@odu.edu>
2025-10-05 01:44:42 -0400

    [INIT] Initial setup

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    SHOOT,
+    COIN
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private AudioSource audioSource;
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+
+    [Header("Shooting")]
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 0.5f;
+    private float nextFireTime = 0f;
+
+    [Header("Audio")]
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleShooting();
+    }
+
+    private void HandleMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical).normalized;
+        rb.linearVelocity = movement * moveSpeed;
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        {
+            FireBullet();
+            nextFireTime = Time.time + fireRate;
+        }
+
+    }
+
+    private void FireBullet()
+    {
+        if (GameManager.Instance.score > 499 && GameManager.Instance.score < 1000)
+            fireRate = 0.3f;
+        if (GameManager.Instance.score > 1000)
+            fireRate = 0.1f;
+
+        if (bulletPrefab && firePoint)
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+
+        AudioManager.PlaySound(SoundType.SHOOT);
+        // Play shoot sound effect
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Player hit by enemy - lose a life
+            GameManager.Instance.LoseLife();
+        }
+
+        if (other.CompareTag("Collectible"))
+        {
+            // Player collected an item
+            Collectible collectible = other.GetComponent<Collectible>();
+            if (collectible)
+            {
+                GameManager.Instance.CollectiblePickedUp(100);
+                AudioManager.PlaySound(SoundType.COIN);
+                Destroy(other.gameObject);
+
+
+            }
+        }
+    }
+}
